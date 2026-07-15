7e51e9781c0f9f1684e0a0f35060c6101c7dfbc8
stanoz <s.oziemczuk@gmail.com>
2026-07-15 21:19:35 +0200

    add car controller

---
+++ backend/src/main/java/com/state/street/backend/controllers/CarController.java
@@ -0,0 +1,31 @@
+package com.state.street.backend.controllers;
+
+import com.state.street.backend.model.dto.CarDto;
+import com.state.street.backend.model.enums.CarCategory;
+import com.state.street.backend.services.CarService;
+import com.state.street.backend.services.CarTypeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(("/cars"))
+@RequiredArgsConstructor
+public class CarController {
+    private final CarService carService;
+    private final CarTypeService carTypeService;
+
+    @GetMapping
+    public ResponseEntity<List<CarDto>> getAllCars() {
+        return ResponseEntity.ok(carService.getAllCars());
+    }
+
+    @GetMapping("/types")
+    public ResponseEntity<List<CarCategory>> getAllCarTypes() {
+        return ResponseEntity.ok(carTypeService.getAllCarTypes());
+    }
+}
