29b65dcfbdf3663c2bd7a0eb53f8cdb8463c1db5
stanoz <s.oziemczuk@gmail.com>
2026-07-15 14:49:24 +0200

    add car service

---
+++ backend/src/main/java/com/state/street/backend/services/CarService.java
@@ -0,0 +1,30 @@
+package com.state.street.backend.services;
+
+import com.state.street.backend.exceptions.car.CarNotFoundException;
+import com.state.street.backend.mappers.CarMapper;
+import com.state.street.backend.model.dto.CarDto;
+import com.state.street.backend.model.dto.ConfirmationCarDto;
+import com.state.street.backend.repositories.CarRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class CarService {
+    private CarRepository carRepository;
+
+    public List<CarDto> getAllCars() {
+        return this.carRepository.findAll()
+                .stream()
+                .map(CarMapper::toCarDto)
+                .toList();
+    }
+
+    public ConfirmationCarDto getCarById(Long carId) throws CarNotFoundException {
+        return carRepository.findById(carId)
+                .map(CarMapper::toConfirmationCarDto)
+                .orElseThrow(() -> new CarNotFoundException(carId));
+    }
+}
