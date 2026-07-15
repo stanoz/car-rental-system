6eb5c02142e0e32c95d7dabb11494e3628fde328
stanoz <s.oziemczuk@gmail.com>
2026-07-15 14:31:43 +0200

    add car mapper, add builder annotation

---
+++ backend/src/main/java/com/state/street/backend/mappers/CarMapper.java
@@ -0,0 +1,27 @@
+package com.state.street.backend.mappers;
+
+import com.state.street.backend.model.dto.CarDto;
+import com.state.street.backend.model.dto.ConfirmationCarDto;
+import com.state.street.backend.model.entity.Car;
+
+public class CarMapper {
+    private CarMapper(){}
+
+    public static CarDto toCarDto(Car carEntity) {
+        return CarDto.builder()
+                .id(carEntity.getId())
+                .brand(carEntity.getBrand())
+                .type(carEntity.getType().getType())
+                .inStock(carEntity.getInStock())
+                .build();
+    }
+
+    public static ConfirmationCarDto toConfirmationCarDto(Car carEntity) {
+        return ConfirmationCarDto.builder()
+                .id(carEntity.getId())
+                .brand(carEntity.getBrand())
+                .type(carEntity.getType().getType())
+                .licensePlate(carEntity.getLicensePlate())
+                .build();
+    }
+}
--- backend/src/main/java/com/state/street/backend/model/dto/ConfirmationCarDto.java
+++ backend/src/main/java/com/state/street/backend/model/dto/ConfirmationCarDto.java
@@ -1,7 +1,9 @@
 package com.state.street.backend.model.dto;
 
 import com.state.street.backend.model.enums.CarCategory;
+import lombok.Builder;
 
+@Builder
 public record ConfirmationCarDto(
         Long id,
         String brand,
