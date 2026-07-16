2d474cd2a61f53dd4dd5a7193b534fcfff2705e3
stanoz <s.oziemczuk@gmail.com>
2026-07-16 14:25:19 +0200

    add missing annotations

--- backend/src/main/java/com/state/street/backend/repositories/CarRepository.java
+++ backend/src/main/java/com/state/street/backend/repositories/CarRepository.java
@@ -2,7 +2,9 @@ package com.state.street.backend.repositories;
 
 import com.state.street.backend.model.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CarRepository extends JpaRepository<Car, Long> {
 
 }
--- backend/src/main/java/com/state/street/backend/repositories/CarTypeRepository.java
+++ backend/src/main/java/com/state/street/backend/repositories/CarTypeRepository.java
@@ -2,6 +2,8 @@ package com.state.street.backend.repositories;
 
 import com.state.street.backend.model.entity.CarType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CarTypeRepository extends JpaRepository<CarType, Long> {
 }
