aa0fc980cf05bfcfed632dfb00a210b8c8f97ff2
stanoz <s.oziemczuk@gmail.com>
2026-07-17 19:47:34 +0200

    refactor findAllByEmailAddress method

--- backend/src/main/java/com/state/street/backend/repositories/UserRepository.java
+++ backend/src/main/java/com/state/street/backend/repositories/UserRepository.java
@@ -4,9 +4,10 @@ import com.state.street.backend.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmailAddress(String emailAddress);
+    List<User> findAllByEmailAddress(String emailAddress);
 }
