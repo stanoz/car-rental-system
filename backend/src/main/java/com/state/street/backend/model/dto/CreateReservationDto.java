7e2164e21208a0da0252a4f9e966f36f2d83db3f
stanoz <s.oziemczuk@gmail.com>
2026-07-18 19:52:56 +0200

    add validation rules

--- backend/src/main/java/com/state/street/backend/model/dto/CreateReservationDto.java
+++ backend/src/main/java/com/state/street/backend/model/dto/CreateReservationDto.java
@@ -1,11 +1,22 @@
 package com.state.street.backend.model.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDateTime;
 
 public record CreateReservationDto(
+        @NotNull(message = "carId cannot be null.")
         Long carId,
+
+        @Valid
+        @NotNull(message = "User cannot be null.")
         UserDto user,
+
+        @NotNull(message = "Start date and time cannot be null.")
         LocalDateTime startDateTime,
+
+        @NotNull(message = "End date and time cannot be null.")
         LocalDateTime endDateTime
 ) {
 }
--- backend/src/main/java/com/state/street/backend/model/dto/UserDto.java
+++ backend/src/main/java/com/state/street/backend/model/dto/UserDto.java
@@ -1,13 +1,35 @@
 package com.state.street.backend.model.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 
 @Builder
 public record UserDto(
+        @NotBlank(message = "First name cannot be blank.")
         String firstName,
+
+        @NotBlank(message = "Last name cannot be blank.")
         String lastName,
+
+        @NotBlank(message = "Driving license ID cannot be blank.")
         String drivingLicenseId,
+
+        @NotBlank(message = "Email address cannot be blank.")
+        @Email(message = "Email address must be valid.")
+        @Pattern(
+                regexp = "^(?!\\.)(?!.*\\.\\.)([a-z0-9_'+\\-.]*)[a-z0-9_+-]@([a-z0-9][a-z0-9-]*\\.)+[a-z]{2,}$",
+                flags = Pattern.Flag.CASE_INSENSITIVE,
+                message = "Email address must be valid."
+        )
         String emailAddress,
+
+        @NotBlank(message = "Phone number cannot be blank.")
+        @Pattern(
+                regexp = "^[0-9]{9}$",
+                message = "Phone number must consist of exactly 9 digits."
+        )
         String phoneNumber
 ) {
 }
