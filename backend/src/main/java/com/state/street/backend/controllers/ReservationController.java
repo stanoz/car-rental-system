46a7e11f65406a13c36118bc852665e5f934ea9b
stanoz <s.oziemczuk@gmail.com>
2026-07-18 20:12:02 +0200

    add ReservationController

---
+++ backend/src/main/java/com/state/street/backend/controllers/ReservationController.java
@@ -0,0 +1,30 @@
+package com.state.street.backend.controllers;
+
+import com.state.street.backend.exceptions.car.CarNotAvailableException;
+import com.state.street.backend.exceptions.car.CarNotFoundException;
+import com.state.street.backend.exceptions.car.InvalidCarStockAmountException;
+import com.state.street.backend.exceptions.reservation.InvalidDatesException;
+import com.state.street.backend.exceptions.user.UserNotFoundException;
+import com.state.street.backend.model.dto.CreateReservationDto;
+import com.state.street.backend.services.ReservationService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reservations")
+@RequiredArgsConstructor
+public class ReservationController {
+    private final ReservationService reservationService;
+
+    @PostMapping
+    public ResponseEntity<Void> createReservation(@Valid @RequestBody CreateReservationDto createReservationDto) throws UserNotFoundException, CarNotAvailableException, InvalidDatesException, InvalidCarStockAmountException, CarNotFoundException {
+        this.reservationService.createReservation(createReservationDto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
