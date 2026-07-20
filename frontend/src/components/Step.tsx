25ad9261f3f7cda5d4aef0a42ed654dafcae45ed
stanoz <s.oziemczuk@gmail.com>
2026-07-20 18:49:29 +0200

    add styling to Step and Steps components

--- frontend/src/components/Step.tsx
+++ frontend/src/components/Step.tsx
@@ -1,18 +1,19 @@
 import type { IconType } from "react-icons";
 
 type StepProps = {
-  isActive: boolean;
-  icon: IconType;
+    isActive: boolean;
+    icon: IconType;
 };
 
 export default function Step({ icon: Icon, isActive }: StepProps) {
-  return (
-    <Icon
-      className={
-        isActive
-          ? "text-blue-600 fill-blue-600"
-          : "text-gray-400 fill-gray-400"
-      }
-    />
-  );
+    return (
+        <Icon
+            size={40}
+            className={
+                isActive
+                    ? "text-emerald-600"
+                    : "text-gray-400"
+            }
+        />
+    );
 }
--- frontend/src/components/Steps.tsx
+++ frontend/src/components/Steps.tsx
@@ -6,16 +6,19 @@ type StepsProps = {
     stepNumber: number
 }
 
+const activeTextClass = "text-emerald-600";
+const inActiveTextClass = "text-gray-400";
+
 export default function Steps({ stepNumber }: StepsProps) {
 
     return (
-        <div>
+        <div className="flex items-center mt-10">
             <Step isActive={stepNumber >= 1} icon={PiNumberCircleOne} />
-            <FaMinus />
+            <FaMinus size={20} className={stepNumber >= 2 ? activeTextClass : inActiveTextClass}/>
             <Step isActive={stepNumber >= 2} icon={PiNumberCircleTwo} />
-            <FaMinus />
+            <FaMinus size={20} className={stepNumber >= 3 ? activeTextClass : inActiveTextClass} />
             <Step isActive={stepNumber >= 3} icon={PiNumberCircleThree} />
-            <FaMinus />
+            <FaMinus size={20} className={stepNumber >= 4 ? activeTextClass : inActiveTextClass}/>
             <Step isActive={stepNumber >= 4} icon={PiNumberCircleFour} />
         </div>
     );
