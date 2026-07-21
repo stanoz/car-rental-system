3b0f27cda5d74b41512b6e9bac0e0e9ba5a3522c
stanoz <s.oziemczuk@gmail.com>
2026-07-21 22:46:50 +0200

    add name prop

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -3,14 +3,15 @@ type InputProps = {
     value: string,
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
     errorMessage: string,
-    label: string
+    label: string,
+    name: string,
 }
 
-export default function Input({ value, onChange, errorMessage, label, id }: InputProps) {
+export default function Input({ value, onChange, errorMessage, label, id, name }: InputProps) {
     return (
         <div className="flex flex-col items-start">
             <label htmlFor={id}>{label}</label>
-            <input id={id} type="text" required value={value} onChange={onChange} />
+            <input id={id} name={name} type="text" required value={value} onChange={onChange} />
             <div className="h-12">
                 <p className="text-red-600 text-xl">{errorMessage}</p>
             </div>
