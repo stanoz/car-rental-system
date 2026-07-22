7b0e34ec8db6f0f7c22f1b2c22e5775eff1eac73
stanoz <s.oziemczuk@gmail.com>
2026-07-22 17:59:47 +0200

    add methods to create and get reservation

---
+++ frontend/src/http/reservation.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import type { ConfirmedReservationDto, CreateReservationDto } from "../model/reservation";
+
+const BASE_URL: string = import.meta.env.VITE_CAR_RENTAL_SYSTEM_BASE_URL;
+
+export async function createReservation(createReservationDto: CreateReservationDto): Promise<number> {
+    return (await axios.post(`${BASE_URL}/reservations`, {
+        ...createReservationDto
+    })).data;
+}
+
+export async function getReservationById(id: number): Promise<ConfirmedReservationDto> {
+    return (await axios.get(`${BASE_URL}/reservations`, {
+        params: {
+            id
+        }
+    })).data;
+}
