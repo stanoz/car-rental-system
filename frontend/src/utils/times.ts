dab8772fada66094e8b7c8a07f5655c455081bd9
stanoz <s.oziemczuk@gmail.com>
2026-07-22 19:35:21 +0200

    add functions to format date and time

--- frontend/src/utils/times.ts
+++ frontend/src/utils/times.ts
@@ -48,3 +48,11 @@ export const times: string[] = [
     "23:00",
     "23:30"
 ];
+
+export function formatDate(date: Date): string {
+    return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, "0")}-${String(date.getDate()).padStart(2, "0")}`;
+}
+
+export function formatTime(date: Date): string {
+    return `${String(date.getHours()).padStart(2, "0")}:${String(date.getMinutes()).padStart(2, "0")}`;
+}
