17888b848bf53cca66da8ad84d17a916622f899e
stanoz <s.oziemczuk@gmail.com>
2026-07-22 16:27:18 +0200

    add create reservation button

--- frontend/src/components/CreateReservationFormContainer.tsx
+++ frontend/src/components/CreateReservationFormContainer.tsx
@@ -30,10 +30,11 @@ export default function CreateReservationFormContainer() {
             {stepNumber === 2 && <ReservationDateTimePicker />}
             {stepNumber === 3 && <UserData />}
             {stepNumber === 4 && <ReservationSummary />}
-            <div className="flex w-96 text-2xl text-cyan-50">
+            <div className="flex w-[28rem] text-2xl text-cyan-50 justify-between">
                 {stepNumber === 1 && <Link to="/" className={`ml-0 mr-auto bg-blue-800 hover:bg-blue-900 ${commonClasses}`}>Return to Cars List</Link >}
                 {stepNumber > 1 && <button type="button" onClick={handleClickPreviousStep} className={`ml-0 mr-auto bg-blue-800 hover:bg-blue-900 ${commonClasses}`}>Previous Step</button>}
                 {stepNumber < 4 && <button disabled={!isStepCompleted} type="button" onClick={handleClickNextStep} className={`mr-0 ml-auto ${isStepCompleted ? "bg-green-700 hover:bg-green-800" : "bg-red-300"}  ${commonClasses}`}>Next Step</button>}
+                {stepNumber == 4 && <button type="button" className={`mr-0 ml-auto bg-green-700 hover:bg-green-800 ${commonClasses}`}>Create Reservation</button>}
             </div>
         </div>
     );
