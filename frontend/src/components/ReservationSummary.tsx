6b71394c407c580e691a0525f64a03c49f708271
stanoz <s.oziemczuk@gmail.com>
2026-07-22 15:35:53 +0200

    add styling

--- frontend/src/components/ReservationSummary.tsx
+++ frontend/src/components/ReservationSummary.tsx
@@ -6,12 +6,12 @@ export default function ReservationSummary() {
     const { car } = useSelectorTyped(state => state.car);
 
     return (
-        <div className="flex flex-col items-center text-cyan-50 text-xl">
+        <div className="flex flex-col items-center text-cyan-50 text-xl my-8">
             <h2 className="text-3xl">Reservation data summary</h2>
-            <main>
+            <main className="mt-6 flex flex-col items-center space-y-6">
                 <section className="flex flex-col items-center">
-                    <h3 className="text-2xl">Car</h3>
-                    <div className="flex flex-col items-start">
+                    <h3 className="text-2xl mb-2">Car</h3>
+                    <div className="flex flex-col items-start space-y-1">
                         <span>Brand: {car.brand}</span>
                         <span>Type: {car.type}</span>
                         <span>Cost per Day: {priceFormatter.format(car.costPerDay)}</span>
@@ -19,21 +19,21 @@ export default function ReservationSummary() {
                     </div>
                 </section>
                 <section className="flex flex-col items-center">
-                    <h3 className="text-2xl">Selected Dates</h3>
+                    <h3 className="text-2xl mb-2">Selected Dates</h3>
                     <h4>From:</h4>
-                    <div className="flex">
+                    <div className="flex mt-1 space-x-6">
                         <span>Date: {dates.fromDate}</span>
                         <span>Time: {dates.fromTime}</span>
                     </div>
                     <h4>To:</h4>
-                    <div className="flex">
+                    <div className="flex mt-1 space-x-6">
                         <span>Date: {dates.toDate}</span>
                         <span>Time: {dates.toTime}</span>
                     </div>
                 </section>
                 <section className="flex flex-col items-center">
-                    <h3 className="text-2xl">Your data</h3>
-                    <div className="flex flex-col items-start">
+                    <h3 className="text-2xl mb-2">Your data</h3>
+                    <div className="flex flex-col items-start space-y-1">
                         <span>Frist name: {user.firstName}</span>
                         <span>Last name: {user.lastName}</span>
                         <span>Email address: {user.emailAddress}</span>
